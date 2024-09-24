8b9644c997b8ca3e43f96655cdb4c628ad8baf4e
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-24 18:13:15 +0300

    41. Практика - Цикл while без блокирования

---
+++ 02-blocking-event-loop/non-blocking-while.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const isRunning = true;
+
+setTimeout(() => isRunning = false, 1);
+process.nextTick(() => console.log('Next tick'));
+
+function setImmediatePromise() {
+    return new Promise((resolve, reject) => {
+        setImmediate(() => resolve());
+        // with resolve()  we stay on the same event loop iteration
+        // resolve()
+    });
+}
+
+async function whileLoop() {
+    while (isRunning) {
+        console.log('while loop is running...');
+        await setImmediatePromise();
+    }
+}
+
+whileLoop().then(() => console.log('While loop ended'));
