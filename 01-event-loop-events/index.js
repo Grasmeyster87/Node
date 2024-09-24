a1ed08faab8e3252d1a18341acbba65414d3455b
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-24 16:31:33 +0300

    37. Практика - Событие DNS и отложенный nextTick

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const dns = require('dns');
 
 function timestamp() {
     return performance.now().toFixed(2);
@@ -6,15 +7,34 @@ function timestamp() {
 
 console.log('Program start');
 
-setTimeout(() => console.log('Timeout 1', timestamp()), 0);
-setTimeout(() => console.log('Timeout 2', timestamp()), 10);
-
+//Close events
 fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written', timestamp()));
 
+// Promises
 Promise.resolve().then(() => console.log('Promise 1', timestamp()));
 
+// Next tick
 process.nextTick(() => console.log('Next tick 1', timestamp()));
 
+// setImmediate (Check)
 setImmediate(() => console.log('Immediate 1', timestamp()));
+
+// Timeouts
+setTimeout(() => console.log('Timeout 1', timestamp()), 0);
+setTimeout(() => {
+    process.nextTick(() => console.log('Next tick 2', timestamp())),
+
+        console.log('Timeout 2', timestamp());
+}, 10);
+
+// Intervals
+setInterval(() => console.log(``))
+
+// I/O Events
+dns.lookup('localhost', (err, address, family) => {
+    console.log('DNS 1 localhost', address, timestamp());
+    Promise.resolve().then(() => console.log('Promise 2', timestamp));
+    process.nextTick(() => console.log('Next tick 3', timestamp()));
+});
 
 console.log('Program end');
