15a06b4af3a15fa9b28ab1e2dd2e5aaa273a0303
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-24 15:15:57 +0300

    35. Практика - Событие окончание записи в файл

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+
 console.log('Program start');
 
 setTimeout(() => console.log('Timeout 1'), 0);
 setTimeout(() => console.log('Timeout 2'), 10);
+
+fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written'));
 
 Promise.resolve().then(() => console.log('Promise 1'));
 
