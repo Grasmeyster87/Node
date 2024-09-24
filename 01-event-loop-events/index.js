fd7c59a7e74957bb01488dd13877caa1e598ad7c
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-24 15:40:36 +0300

    36 Практика - Добавление события nextTick

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 
+function timestamp() {
+    return performance.now().toFixed(2);
+}
+
 console.log('Program start');
 
-setTimeout(() => console.log('Timeout 1'), 0);
-setTimeout(() => console.log('Timeout 2'), 10);
+setTimeout(() => console.log('Timeout 1', timestamp()), 0);
+setTimeout(() => console.log('Timeout 2', timestamp()), 10);
 
-fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written'));
+fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written', timestamp()));
 
-Promise.resolve().then(() => console.log('Promise 1'));
+Promise.resolve().then(() => console.log('Promise 1', timestamp()));
+
+process.nextTick(() => console.log('Next tick 1', timestamp()));
+
+setImmediate(() => console.log('Immediate 1', timestamp()));
 
 console.log('Program end');
