d33c4625e01f3befaae336260b64b771ef3107ce
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-24 17:33:18 +0300

    39. Практика - Оптимизация кода с событиями

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,44 +1,43 @@
 const fs = require('fs');
 const dns = require('dns');
 
-function timestamp() {
-    return performance.now().toFixed(2);
+function info(text) {
+    console.log(text, performance.now().toFixed(2));
 }
 
 console.log('Program start');
 
 //Close events
-fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written', timestamp()));
+fs.writeFile('./test.txt', 'Hello Node.js', () => info('File written'));
 
 // Promises
-Promise.resolve().then(() => console.log('Promise 1', timestamp()));
+Promise.resolve().then(() => info('Promise 1'));
 
 // Next tick
-process.nextTick(() => console.log('Next tick 1', timestamp()));
+process.nextTick(() => info('Next tick 1'));
 
 // setImmediate (Check)
-setImmediate(() => console.log('Immediate 1', timestamp()));
+setImmediate(() => info('Immediate 1'));
 
 // Timeouts
-setTimeout(() => console.log('Timeout 1', timestamp()), 0);
+setTimeout(() => info('Timeout 1'), 0);
 setTimeout(() => {
-    process.nextTick(() => console.log('Next tick 2', timestamp())),
-
-        console.log('Timeout 2', timestamp());
+    process.nextTick(() => info('Next tick 2')),
+    info('Timeout 2');
 }, 100);
 
 // Intervals
 let intervalCount = 0;
 const intervalId = setInterval(() => {
-    console.log(`Inteval ${intervalCount += 1}`, timestamp());
+    info(`Inteval ${intervalCount += 1}`);
     if (intervalCount === 2) clearInterval(intervalId);
 }, 50);
 
 // I/O Events
 dns.lookup('localhost', (err, address, family) => {
-    console.log('DNS 1 localhost', address, timestamp());
-    Promise.resolve().then(() => console.log('Promise 2', timestamp));
-    process.nextTick(() => console.log('Next tick 3', timestamp()));
+    info('DNS 1 localhost');
+    Promise.resolve().then(() => info('Promise 2'));
+    process.nextTick(() => info('Next tick 3'));
 });
 
 console.log('Program end');
