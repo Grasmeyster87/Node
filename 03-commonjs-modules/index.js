805d61259e33893130bb2ee37e9bbcc0e176d3f7
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-27 16:58:03 +0300

    59. Практика - Подведение итогов по модулям CommonJS

--- 03-commonjs-modules/index.js
+++ 03-commonjs-modules/index.js
@@ -6,7 +6,7 @@ console.log('Vitaliy');
 
 const { myName, myHobbies, myFavoriteNumber } = require('./multiple-export');
 const { myName: myOtherName, myFriendsName, myGreatHobbies } = require('./export-and-import');
-const greetingFn = require('./single-export');
+const greetingFn = require('./my-modules/single-export');
 
 //const greetingFn = require('/xampp/htdocs/Node/03-commonjs-modules/single-export.js');
 
@@ -15,6 +15,7 @@ console.log(myName);
 console.log(myHobbies);
 console.log(myFavoriteNumber);
 
+// mutates array in the multiple-exports modules!
 myHobbies.push('climbing');
 
 // Import from single-export
