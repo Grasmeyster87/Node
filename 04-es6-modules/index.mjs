cbc920c26154e09c9050e862f8de4ef3e03a36d5
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-28 12:53:00 +0300

    65. Практика - Экспорт по умолчанию в ES6

--- 04-es6-modules/index.mjs
+++ 04-es6-modules/index.mjs
@@ -1,7 +1,12 @@
 import { season, temperature } from "./named-exports.mjs";
 import { isRainig, humidity } from "./inline-exports.mjs";
+import getDataFromServer from './default-export.mjs';
 
 console.log(season);
 console.log(temperature);
 
 console.log(isRainig, '\n', humidity);
+
+getDataFromServer('https://jsonplaceholder.typicode.com/posts/1')
+    .then((post) => console.log(post))
+    .catch((err) => console.error(err));
