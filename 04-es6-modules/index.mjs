b6fa9973475e56105c93c502d4600033a3862b12
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-28 14:32:51 +0300

    67. Практика - Импорт именованных экспортов и экспорта по умолчанию

--- 04-es6-modules/index.mjs
+++ 04-es6-modules/index.mjs
@@ -1,6 +1,7 @@
 import { season, temperature } from "./named-exports.mjs";
 import { isRainig, humidity } from "./inline-exports.mjs";
 import getDataFromServer from './default-export.mjs';
+import DEFAULT_SERVER, { USERNAME as MY_USERNAME, PASSWORD as MY_USERNAME } from "./mixed.exports.mjs";
 
 console.log(season);
 console.log(temperature);
@@ -10,3 +11,6 @@ console.log(isRainig, '\n', humidity);
 getDataFromServer('https://jsonplaceholder.typicode.com/posts/1')
     .then((post) => console.log(post))
     .catch((err) => console.error(err));
+
+console.log(DEFAULT_SERVER);
+console.log(USERNAME, PASSWORD);
