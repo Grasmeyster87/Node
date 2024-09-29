f2b6b39f7d540b5b43536390f1b8fd565522a7a3
Grasmeyster87 <grasmeyster@gmail.com>
2024-09-29 13:01:59 +0300

    Раздел 10 - Модуль fs - 74. Практика - Модуль fs и версии функций с колбэками

---
+++ 05-fs/promises-fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+fs.writeFile('./05-fs/first.txt', 'First file text', (err) => {
+    if (err) console.log(err);
+    else {
+        console.log('File first.txt was written');
+
+        fs.appendFile('./05-fs/first.txt', '\nOne more line', (err) => {
+            if (err) console.log(err);
+            else {
+                console.log('Append text to the first.txt file');
+                fs.rename('./05-fs/first.txt', './05-fs/renamed-first.txt', (err) => {
+                    if (err) console.log(err);
+                    else console.log('File was renamed');
+                });
+            }
+        });
+
+    }
+});
