5062287ae444d44cf1ecddcb3df64c588aa15d9f
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-01 17:22:12 +0300

    85. Практика - Регистрация слушателя в конструкторе

--- 06-events/class-events.mjs
+++ 06-events/class-events.mjs
@@ -6,9 +6,18 @@ class Post extends EventEmitter {
         this.author = author;
         this.text = text;
         this.likesQty = 0;
+        this.on('likePost', (username) => {
+            console.log(`${username} liked your post`);
+        });
+        this.on('error', (error) => {
+            console.error(error);
+        });
     }
 
     like(username) {
+        if (!username) {
+            return this.emit('error', new Error('No username in the like request!'));
+        }
         this.likesQty += 1;
         this.emit('likePost', username);
     }
@@ -17,14 +26,12 @@ class Post extends EventEmitter {
 
 const myPost = new Post('Bogdan', 'My great post!');
 
-myPost.on('likePost', (username) => {
-    console.log(`${username} liked your post`);
-});
+
 
 // console.log(myPost.author);
 // console.log(myPost.text);
 // console.log(myPost.likesQty);
 myPost.like('Alice');
-setTimeout(() => myPost.like('Bob'), 1000);
+setTimeout(() => myPost.like(), 1000);
 setTimeout(() => myPost.like('Alex'), 2000);
 //console.log(myPost.likesQty);;
