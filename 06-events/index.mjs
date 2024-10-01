0eaff754e6a6608a9671b40ba3cb81e7d428c05c
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-01 09:56:25 +0300

    80. Практика - Однократная реакция на событие

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -9,3 +9,10 @@ myEmitter.on('timeout', (secondsQty) => {
 setTimeout(() => { myEmitter.emit('timeout', 1); }, 1000);
 setTimeout(() => { myEmitter.emit('timeout', 2); }, 2000);
 
+myEmitter.once('singleEvent', () => {
+    console.log('Single event occurred');
+});
+
+// Listener function will be called only once!
+setTimeout(() => { myEmitter.emit('singleEvent'); }, 500);
+setTimeout(() => { myEmitter.emit('singleEvent'); }, 1500);
