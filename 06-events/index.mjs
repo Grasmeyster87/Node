55d709f2c954dabc8c718e1d413e0b5cab7d0bd3
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-01 09:50:50 +0300

    79. Практика - Передача значений параметров в слушателей событий

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,10 +2,10 @@ import { EventEmitter } from 'events';
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('timeout', () => {
-    console.log('Timeout event!');
+myEmitter.on('timeout', (secondsQty) => {
+    console.log(`Timeout event in ${secondsQty} seconds!`);
 });
 
-setTimeout(() => { myEmitter.emit('timeout'); }, 1000);
-setTimeout(() => { myEmitter.emit('timeout'); }, 2000);
+setTimeout(() => { myEmitter.emit('timeout', 1); }, 1000);
+setTimeout(() => { myEmitter.emit('timeout', 2); }, 2000);
 
