3b533b05ec93287a26d89a5685d20748d5d2919f
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-01 14:18:38 +0300

    83. Практика - Запись в файл с помощью EventEmitter

---
+++ 06-events/fs-events.mjs
@@ -0,0 +1,35 @@
+import { EventEmitter } from 'events';
+import fs from 'fs';
+
+const fileEmitter = new EventEmitter();
+
+const filePath = './06-events/first.txt';
+
+fileEmitter.on('writeComplete', () => {
+    console.log(`File ${filePath} was written`);
+    fs.appendFile(filePath, '\nOne more line', () => {
+        fileEmitter.emit('appendComplete');
+    });
+});
+
+fileEmitter.on('appendComplete', () => {
+    console.log(`Append text to the ${filePath} file`);
+    fs.rename(filePath, './06-events/renamed-first.txt', () => {
+        fileEmitter.emit('renameComlet');
+    });
+    console.log('File was renamed');
+});
+
+fileEmitter.on('renameComplete', () => {
+    console.log('File first.txt was written');
+});
+
+fs.writeFile(filePath, 'first file text', () => {
+    fileEmitter.emit('writeComplete');
+});
+
+
+
+
+
+
