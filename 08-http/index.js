35750b4c6c1906daeb35196b02832c937c721042
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-03 10:46:33 +0300

    91. Практика - Изменение объекта res и тип контента

--- 08-http/index.js
+++ 08-http/index.js
@@ -2,18 +2,31 @@ const http = require('http');
 
 const PORT = 5000;
 
+const comments = [
+    { id: 100, text: 'First commet', author: 'Bogdan' },
+    { id: 526, text: 'Second commet', author: 'Alice' },
+    { id: 724, text: 'Last commet', author: 'BoB ' },
+];
+
 const server = http.createServer((req, res) => {
     // console.log(req);
-    if (req.url === '/http') {
+    if (req.url === '/html') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        //res.write(<html><body></body></html>)
-        return res.end('<h1>Greatings from the HTTP server!</h1>');
+        res.write('<html><body><div>');
+        res.write('<h1>Greatings from the HTTP server!</h1>');
+        res.write('</div></body></html>');
+        return res.end();
     }
-    if (req.url === 'text') {
+    if (req.url === '/text') {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text-plain');
+        res.setHeader('Content-Type', 'text/plain');
         return res.end('This is plain text');
+    }
+    if (req.url === '/json') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(comments));
     }
 });
 
