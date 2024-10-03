1e04c520233f2490cf14b8c9904752c06acb8500
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-03 09:50:21 +0300

    91. Практика - Изменение объекта res и тип контента

--- 08-http/index.js
+++ 08-http/index.js
@@ -3,8 +3,18 @@ const http = require('http');
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Greatings from the HTTP server!');
+    // console.log(req);
+    if (req.url === '/http') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        //res.write(<html><body></body></html>)
+        return res.end('<h1>Greatings from the HTTP server!</h1>');
+    }
+    if (req.url === 'text') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text-plain');
+        return res.end('This is plain text');
+    }
 });
 
 server.listen(PORT, () => {
