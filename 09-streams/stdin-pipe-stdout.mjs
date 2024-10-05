eeb3a1efad6ed3277ea819284b4d39d0f486e298
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-05 16:04:07 +0300

    108. Практика - Трансформационный поток

--- 09-streams/stdin-pipe-stdout.mjs
+++ 09-streams/stdin-pipe-stdout.mjs
@@ -1,11 +1,21 @@
-import stream from 'stream';
+import { Transform } from 'stream';
 import fs from 'fs';
 
+const upperCaseStream = new Transform({
+    transform: function (chunk, encoding, cb) {
+        const upperCased = chunk.toString().toUpperCase(); // convert buffer to string
+        //console.log(upperCased);
+        cb(null, upperCased);
+    }
+});
+
+process.stdin.pipe(upperCaseStream).pipe(process.stdout);
+
 // Pipe to file
-const filePath = './files/stdin-dump.txt';
+/*const filePath = './files/stdin-dump.txt';
 const writeStream = fs.createWriteStream(filePath);
 process.stdin.pipe(writeStream);
-
+*/
 // Pipe to stdout
 // process.stdin.pipe(process.stdout);
 
