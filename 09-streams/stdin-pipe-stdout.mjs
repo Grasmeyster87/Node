56a30b204010297038d2c76ee354cb0037374a72
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-05 16:28:04 +0300

    110. РЕШЕНИЕ - Добавление трансформационного потока

--- 09-streams/stdin-pipe-stdout.mjs
+++ 09-streams/stdin-pipe-stdout.mjs
@@ -9,7 +9,19 @@ const upperCaseStream = new Transform({
     }
 });
 
-process.stdin.pipe(upperCaseStream).pipe(process.stdout);
+const reverseStream = new Transform({
+    transform(chunk, encoding, cb) {
+        const arrayOfChars = chunk.toString().split('');
+        const lastChar = arrayOfChars.pop();
+        const reversed = arrayOfChars.reverse().concat(lastChar).join('');
+        cb(null, reversed);
+    }
+});
+
+process.stdin
+    .pipe(upperCaseStream)
+    .pipe(reverseStream)
+    .pipe(process.stdout);
 
 // Pipe to file
 /*const filePath = './files/stdin-dump.txt';
