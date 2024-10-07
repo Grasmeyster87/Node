e620a8c29fabb026e88d50214c061924acb80282
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-07 19:33:32 +0300

    116. Практика - Продолжение копирования папки с файлами

--- 09-streams/copy-file-in-folder.mjs
+++ 09-streams/copy-file-in-folder.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 
 const sourceDir = './files';
 const destinationDir = './copied-files';
@@ -10,7 +11,31 @@ if (!fs.existsSync(sourceDir)) {
 }
 
 if (fs.existsSync(destinationDir)) {
-    fs.rmdirSync(destinationDir);
+    fs.rmSync(destinationDir, { recursive: true });
     console.log('Destination dir removed');
 }
 fs.mkdirSync(destinationDir);
+
+fs.readdir(sourceDir, (err, fileNames) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Start', performance.now());
+    //console.log(fileNames);
+
+    fileNames.forEach((fileName, index) => {
+        const sourceFilePath = path.join(sourceDir, fileName);
+        const destinationFilePath = path.join(destinationDir, `${index + 1}. ${fileName} `); // 1. index.html
+
+        const readFileStream = fs.createReadStream(sourceFilePath);
+        const writeFileStream = fs.createWriteStream(destinationFilePath);
+
+        readFileStream.pipe(writeFileStream);
+
+        writeFileStream.on('finish', () => {
+            console.log(`File ${fileName} was copied`);
+        });
+    });
+    console.log('End', performance.now());
+});
