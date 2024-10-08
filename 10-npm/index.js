ea87730f372ca732d614cda1263c8f4d9f7424f4
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-08 17:51:19 +0300

    131. Практика - NPM скрипты

--- 10-npm/index.js
+++ 10-npm/index.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const app = express();
 
-console.log(express);
+app.get('/', function (req, res) {
+    res.send('Hello from Express!!!');
+});
+
+app.listen(3000);
