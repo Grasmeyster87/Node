6d192e637d43fbf14b84cb4196972201b2cbc83e
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-11 15:27:26 +0300

    156. Практика - Подведение итогов по роутингу + /

--- 12-express/03-routs/app.js
+++ 12-express/03-routs/app.js
@@ -5,6 +5,7 @@ const router = require('./routes/index');
 
 const app = express();
 app.use(router);
+//app.use('/', router)
 
 app.listen(5000, () => console.log('Server was started on port 5000'));
 
