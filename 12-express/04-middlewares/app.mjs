16ce52d94342412bfff6d8907262c28eebd2ded4
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-12 17:05:31 +0300

    162. Практика - Парсинг JSON от клиента с помощью middleware

--- 12-express/04-middlewares/app.mjs
+++ 12-express/04-middlewares/app.mjs
@@ -17,17 +17,24 @@ app.get('/', logger, () => {res.send(...)})
 app.use(logger (req, res) => res.send('This is express server'));
 
 */
-app.use(morgan('combined'));
+app.use(morgan('tiny')); //tiny, combined, common, dev, short, tiny
 
-app.use((req, res, next) => {
+app.use(express.json());
+
+/* app.use((req, res, next) => {
     //console.log(req);
     let data = '';
     req.on('data', (chunk) => (data += chunk));
-    req.on('end', () => console.log(JSON.parse(data)));
-    next();
+    req.on('end', () => {
+        const parsedJSON = JSON.parse(data);
+        req.body = parsedJSON;
+        next();
+    });
+}); */
+
+app.use((req, res) => {
+    console.log(req.body);
+    return res.send('This is express server');
 });
-
-app.use((req, res) => res.send('This is express server'));
-
 
 app.listen(5000, () => console.log('server is listening at port 5000'));
