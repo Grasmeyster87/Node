0b0f411c6fe0205a869f87f96ad805ccfecf9666
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-12 16:25:44 +0300

    160. Практика - Логирование с помощью morgan

--- 12-express/04-middlewares/app.mjs
+++ 12-express/04-middlewares/app.mjs
@@ -1,16 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+/* 
 
 const logger = (req, res, next) => {
     console.log(req.method, req.path);
     next();
 };
 
-app.use(logger);
-// app.get('/', logger, () => {res.send(...)})
-//app.use(logger (req, res) => res.send('This is express server'));
-app.use( (req, res) => res.send('This is express server'));
+app.use(logger); 
+
+app.get('/', logger, () => {res.send(...)})
+
+app.use(logger (req, res) => res.send('This is express server'));
+
+*/
+app.use(morgan('combined'));
+
+app.use((req, res) => res.send('This is express server'));
 
 
 app.listen(5000, () => console.log('server is listening at port 5000'));
