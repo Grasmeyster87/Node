dc5dcd34d8f32181a3043e16f248450ff457b6c3
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-12 18:21:53 +0300

    166. Практика - Резюме по использованию middleware функций

--- 12-express/04-middlewares/app.mjs
+++ 12-express/04-middlewares/app.mjs
@@ -3,8 +3,12 @@ import morgan from 'morgan';
 
 const app = express();
 
+// logs info about request
 app.use(morgan('tiny')); //tiny, combined, common, dev, short, tiny
+// converts JSON  to  JS Object in POST, PUT, PATCH request
 app.use(express.json());
+
+//convert from data to JS Object in POST, PUT, PATCH requsts
 // конвертирование отправленных на сервер данных форм в обект JS 
 // extended: true использум внешний модуль qs
 app.use(express.urlencoded({ extended: true }));
